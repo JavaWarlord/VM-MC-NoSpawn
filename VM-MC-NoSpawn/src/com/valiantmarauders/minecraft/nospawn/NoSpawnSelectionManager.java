29c354b29ff44bca5ca091955e65553d2adeb29e
JavaWarlord <JavaWarlord@gmail.com>
2013-12-27 11:45:58 -0500

    OK, fixed it back to selecting with Material instead of ItemStack

--- VM-MC-NoSpawn/src/com/valiantmarauders/minecraft/nospawn/NoSpawnSelectionManager.java
+++ VM-MC-NoSpawn/src/com/valiantmarauders/minecraft/nospawn/NoSpawnSelectionManager.java
@@ -3,7 +3,6 @@ package com.valiantmarauders.minecraft.nospawn;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.valiantmarauders.minecraft.block.BlockClickHandler;
@@ -13,14 +12,14 @@ import com.valiantmarauders.minecraft.selection.SelectionManager;
 public class NoSpawnSelectionManager extends CuboidSelectionManager implements
 		SelectionManager, BlockClickHandler {
 
-	public NoSpawnSelectionManager(JavaPlugin plugin, Material selectionWand) {
-		super(plugin, selectionWand);
+	public NoSpawnSelectionManager(JavaPlugin plugin, Material material) {
+		super(plugin, material);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public void notify(Player player, ItemStack item, Block block) {
+	public void notify(Player player, Material material, Block block) {
 		// TODO Auto-generated method stub
-		addPoint(player, item, block);
+		addPoint(player, material, block);
 	}
 }
