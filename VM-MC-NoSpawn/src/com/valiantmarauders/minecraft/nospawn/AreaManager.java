40669aac65cdcff3e8718586c94a5e6fae925725
JavaWarlord <JavaWarlord@gmail.com>
2013-12-27 09:53:34 -0500

    Check if save file exists

--- VM-MC-NoSpawn/src/com/valiantmarauders/minecraft/nospawn/AreaManager.java
+++ VM-MC-NoSpawn/src/com/valiantmarauders/minecraft/nospawn/AreaManager.java
@@ -3,6 +3,7 @@ package com.valiantmarauders.minecraft.nospawn;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
@@ -53,6 +54,14 @@ public class AreaManager {
 	public void save() {
 		// TODO Auto-generated method stub
 		File file = new File("plugins/NoSpawn/" + fileName);
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		try {
 			ObjectOutputStream oos = new ObjectOutputStream(
 					new FileOutputStream(file.getAbsolutePath()));
